e7ae495392f7c31cfee2a20ced90d49dd56ac4b4
Robert Lowstetter <bobby.lowstetter@banno.com>
2024-07-01 11:23:15 -0400

    Start migration to ServiceNow

--- triage/main.py
+++ triage/main.py
@@ -3,7 +3,7 @@ import os
 import logging
 from slack_bolt import App
 
-# Jira API stuff
+# Jira API stuff for Future Features
 import http.client
 import json
 
@@ -20,6 +20,9 @@ handler = SlackRequestHandler(app)
 #endregion
 
 #region Shared Variables
+# ServiceNow API stuff
+
+# Jira API stuff for Future Features
 jira_authorization=os.environ.get("JIRA_AUTH")
 #endregion
 
@@ -28,20 +31,10 @@ jira_authorization=os.environ.get("JIRA_AUTH")
 @app.command("/triage")
 def hello_command(ack, logger):
   logger.info("I see a slash command!")
-  ack("Hi from The Jira and Slack Integration bot! Use reactions to have me create (:eyes) or complete (:white_check_mark) a ticket.")
+  ack("Hi from The Pulsar Slack Integration bot! Use reactions to have me create (:eyes) or complete (:white_check_mark) a ticket.")
 #endregion
 
-#region Reaction_Removed Event Functionality
-# @app.event("reaction_removed")
-# def handle_reaction_removed(body, say, logger):
-#   logger.info("I see a reaction_removed event!")
-#   logger.info(body)
-#   reaction = body["event"]["reaction"]
-#   if reaction == "eyes":
-#     say("You removed the :eyes: reaction. I will delete that ticket.")
-#   if reaction == "white_check_mark":
-#     say("You removed the :white_check_mark: reaction. I will reopen that ticket.")
-#endregion
+# TODO: Handle if a reaction is removed (Future Feature)
 
 #region Reaction_Added Event Functionality
 @app.event("reaction_added")
@@ -72,49 +65,26 @@ def handle_reaction_added(body, say, logger):
   )["messages"][0]["text"]
   #endregion
   
-  # TODO: Don't allow this to be run from within a thread
+  # TODO: Don't allow this to be run from within a thread (Future Feature)
   
-  #region :eyes: Reaction Functionality (Create a ticket)
-  if reaction == "eyes":    
-    # Confirmed that reporter_email and assignee_email is good. Using a dummy for testing.
-    # TODO: Remove the dummy 
-    reporter_email = "blowstetter@jackhenry.com"
-    assignee_email = "blowstetter@jackhenry.com"
-      
-    # The Assignee should always be in Jira, so we will error if they are not found
-    try:
-      jiraAssigneeID = findJiraUserAccountID(assignee_email)
-    except:
-      say(f"Could not find <@{assignee}> in Jira. Please make sure your email is in Jira.", thread_ts=body["event"]["item"]["ts"])
-      return
+  #region :jh: Reaction Functionality (Create a ticket)
+  if reaction == "jh":
     
-    try:
-      jiraReporterID = findJiraUserAccountID(reporter_email)
-    except:
-      # If the reporter is not found in Jira, we will the assignee to create the ticket
-      say(f"Could not find <@{reporter}> in Jira. Using <@{assignee}> as Reporter.", thread_ts=body["event"]["item"]["ts"])
-      jiraReporterID = jiraAssigneeID
-      
-    # TODO: Handle is the user is not found in Jira (only matters if they are not the assignee)
-    try:
-      ticketURL = createTicket(url, description, jiraReporterID, jiraAssigneeID)
-      say(f"Ticket created: {ticketURL}.", thread_ts=body["event"]["item"]["ts"])
-    except:
-      say("There was an error creating the ticket.", thread_ts=body["event"]["item"]["ts"])
+    # TODO: Create a function to create a ticket (Needed Feature)
+    say(f"Creating a ticket for {assignee_email}...")
     #endregion
   
   #region :white_check_mark: Reaction Functionality (Complete a ticket)
-  elif reaction =="white_check_mark":
-    say(f"Hey there <@{assignee}>! I will mark this ticket as done!", thread_ts=body["event"]["item"]["ts"])
+  # TODO: Create a function to complete a ticket (Future Feature)
   #endregion
 
 #endregion
 
-#region App_Mention (@Jira and Slack Integration) Event Functionality
+#region App_Mention (@Pulsar Slack Integration) Event Functionality
 @app.event("app_mention")
 def event_test(say, logger):
   logger.info("I see a mention!")
-  say("Hi there! I am The Jira and Slack Integration bot! Use reactions to have me create (:eyes) or complete (:white_check_mark) a ticket.")
+  say("Hi there! I am The Pulsar Slack Integration bot! Use reactions to have me create (:jh) a ticket.")
 
 def triage(req: Request):
   app = App(
@@ -124,67 +94,9 @@ def triage(req: Request):
   return handler.handle(req)
 #endregion
 
-#region Jira API Functions
-def findJiraUserAccountID(email):
-  # This is the Jira API for finding a user's account ID
-  conn = http.client.HTTPSConnection("banno-jha.atlassian.net")
-  payload = ''
-  headers = {
-    'Authorization': 'Basic ' + jira_authorization
-  }
-  conn.request("GET", f"/rest/api/3/user/search?query=" + email, payload, headers)
-  res = conn.getresponse()
-  data = res.read()
-  # Parse the JSON and return the account ID
-  return json.loads(data.decode("utf-8"))[0]["accountId"]
+#region ServiceNow API Functions
 
-def createTicket(url, description, jiraReporterID, jiraAssigneeID):
-  conn = http.client.HTTPSConnection("banno-jha.atlassian.net")
-  payload = json.dumps({
-    "fields": {
-      "project": {
-        "key": "PULS"
-      },
-      "summary": url,
-      "description": {
-        "type": "doc",
-        "version": 1,
-        "content": [
-          {
-            "type": "paragraph",
-            "content": [
-              {
-                "type": "text",
-                "text": description
-              }
-            ]
-          }
-        ]
-      },
-      "issuetype": {
-        "name": "Task"
-      },
-      "reporter": {
-        "id": jiraReporterID
-      },
-      "assignee": {
-        "accountId": jiraAssigneeID
-      }
-    }
-  })
-  headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Basic ' + jira_authorization
-  }
-  conn.request("POST", "/rest/api/3/issue", payload, headers)
-  res = conn.getresponse()
-  data = res.read()
-  print(data.decode("utf-8"))
-  
-  # Get the ticket number
-  ticketNumber = json.loads(data.decode("utf-8"))["key"]
-  
-  # Return the URL of the ticket
-  return f"https://banno-jha.atlassian.net/browse/{ticketNumber}"
+# TODO: Create a function to create a ticket (Needed Feature)
+# TODO: Create a function to complete a ticket (Future Feature)
 
 #endregion
